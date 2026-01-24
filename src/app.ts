bbc2eb2ec9e8b6af8b5674a3c926f7b179b71fb5
salekmasudparvez1 <smp23997@gmail.com>
2026-01-24 22:18:39 +0600

    Add render.yaml

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
-        Message: "Ph health care server.."
+        Message: "Welcome to Bike Service Management Server",
     })
 });
 app.use('/api', router)
