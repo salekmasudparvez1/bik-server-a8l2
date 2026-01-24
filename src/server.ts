f7c8c28664b67f11968663a498998a1786d0d05c
salekmasudparvez1 <smp23997@gmail.com>
2026-01-24 21:03:45 +0600

    fix:serverless func

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import { Server } from 'http';
 
 
-import config from './app/config'; 
-import app from './app';
+import config from './app/config/index.js'; 
+import app from './app.js';
 
 async function main() {
-  // Only run the server locally, Vercel will handle it in production
   if (process.env.NODE_ENV !== 'production') {
     const server: Server = app.listen(config.port, () => {
       console.log('Server is running on port', config.port);
